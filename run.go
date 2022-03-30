428925ab1c828213e0bf23d60745330c939d3ccf
slyflash <807071409@qq.com>
2022-03-31 01:01:56 +0800

    增加参数区分本地测试还是集群部署

--- run.go
+++ run.go
@@ -19,17 +19,24 @@ import (
 var hallRpcPortFlag = flag.String("hallRpcPort", ":8000", "set the port of rpc in order to communicate with hall")
 var clientTcpAddrFlag = flag.String("clientAddr", "49.234.245.172:32004", "set the port of tcp in order to communicate with client")
 var etcdAddrFlag = flag.String("etcdAddr", "42.192.200.194:2379", "set the address of etcd")
+var testFlag = flag.Bool("t", false, "if this is a local test")
 
 func ParseFlags() {
 	flag.Parse()
-	server.GS.HallRpcPort = *hallRpcPortFlag
-	server.GS.ClientAddr = *clientTcpAddrFlag
-	configs.EtcdAddr = *etcdAddrFlag
-	server.EtcdCli = server.NewEtcdCli()
+	if *testFlag {
+		//如果当前是本机测试
+		*hallRpcPortFlag = ":8000"
+		*clientTcpAddrFlag = "localhost:8001"
+		*etcdAddrFlag = "42.192.200.194:2379"
+	}
 }
 
 func main() {
 	ParseFlags()
+	server.GS.HallRpcPort = *hallRpcPortFlag
+	server.GS.ClientAddr = *clientTcpAddrFlag
+	configs.EtcdAddr = *etcdAddrFlag
+	server.EtcdCli = server.NewEtcdCli()
 	server.GS.Run()
 	logger.Infof("开启game server:大厅服务器rpc端口[%s],客户端kcp地址[%s]\n", *hallRpcPortFlag, *clientTcpAddrFlag)
 	time.Sleep(100 * time.Minute)

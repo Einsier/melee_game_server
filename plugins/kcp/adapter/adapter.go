3152dae4dbc8476c62ffd53f7e0d9e255184e658
slyflash <807071409@qq.com>
2022-04-01 16:36:34 +0800

    取消打印所有包内容

--- plugins/kcp/adapter/adapter.go
+++ plugins/kcp/adapter/adapter.go
@@ -1,7 +1,6 @@
 package adapter
 
 import (
-	"fmt"
 	"log"
 	pb "melee_game_server/api/client/proto"
 	codec "melee_game_server/plugins/kcp/codec"
@@ -20,7 +19,7 @@ func Send(conn net.Conn, msg *pb.TopMessage) {
 	if err != nil {
 		log.Println("Usage:Send()调用失败，错误信息:", err)
 	} else {
-		fmt.Printf("%v\n", msg)
+		//fmt.Printf("%v\n", msg)
 		//fmt.Println("Msg:Send()调用成功，共发送", count, "字节")
 	}
 }

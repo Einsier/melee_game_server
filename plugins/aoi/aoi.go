e1ff765a72e12013858ac2db054cbee3dc05fa1d
slyflash <807071409@qq.com>
2022-04-15 22:28:54 +0800

    还是改成二维数组了(狗头)

--- plugins/aoi/aoi.go
+++ plugins/aoi/aoi.go
@@ -4,7 +4,6 @@ import (
 	"melee_game_server/framework/entity"
 	"melee_game_server/plugins/aoi/object"
 	"melee_game_server/plugins/logger"
-	"sync"
 	"time"
 )
 
@@ -15,66 +14,69 @@ import (
 *@Description:1.0版本 暂时将地图宽度和高度,格子宽度高度设置为整形,方便进行各种取模运算
 大概思想来自:https://blog.csdn.net/qq_36748278/article/details/102787225
 为了效率,很多固定的运算提前的进行了运算,例如每个格子的四周格子,每个格子的邻接格子等等
- */
+*/
 
 type MapInfo struct {
-	MX    int            //地图宽度
-	MY    int            //地图高度
-	GX    int            //每个格子的宽度
-	GY    int            //每个格子的高度
-	NGX   int            //X轴上有多少个格子
-	NGY   int            //Y轴上有多少个格子
-	NGrid int            //一共有多少个格子
-	Grids []*object.Grid //被上面的博客带跑偏了= =感觉还是二维数组比较好写 而且性能也一样的= =
+	MX    int              //地图宽度
+	MY    int              //地图高度
+	GX    int              //每个格子的宽度
+	GY    int              //每个格子的高度
+	NGX   int              //X轴上有多少个格子
+	NGY   int              //Y轴上有多少个格子
+	NGrid int              //一共有多少个格子
+	Grids [][]*object.Grid //被上面的博客带跑偏了= =感觉还是二维数组比较好写 而且性能也一样的= =
 }
 
 type HeroInfo struct {
-	HeroNum			int
-	Speed			float64				//每ms移动多少m
-	UpdateDuration	time.Duration
+	HeroNum        int
+	Speed          float64 //每ms移动多少m
+	UpdateDuration time.Duration
 
-	heroLock		sync.RWMutex
-	heroes			map[int32]*Hero
+	Heroes map[int32]*Hero
+}
+
+type MsgChan struct {
+	Ticker *time.Ticker      //定期发包的定时器
+	Quit   chan *HeroQuitMsg //英雄退出的chan
+	Move   chan *HeroMoveMsg //英雄移动的chan
+	Finish chan struct{}
 }
 
 type AOI struct {
 	*MapInfo
 	*HeroInfo
-	ticker *time.Ticker
+	*MsgChan
 }
 
 //GetGridByPos 获取当前位置应该属于哪个grid,并从中取出grid
-func (mp *MapInfo)GetGridByPos(pos entity.Vector2)*object.Grid{
-	return mp.Grids[(int(pos.Y) / mp.GY) * mp.NGX + (int(pos.X) / mp.GX)]
-}
-
-//GetGridById 通过id获取grid,如果越界返回nil,如果create = true说明如果不存在,创建一个
-func (mp *MapInfo)GetGridById(id int)*object.Grid{
-	if id < 0 || id >= mp.NGrid{
+func (mp *MapInfo) GetGridByPos(pos *entity.Vector2) *object.Grid {
+	if !mp.checkPositionLegal(pos) {
+		logger.Errorf("position:%+v is illegal", pos)
 		return nil
 	}
-	return mp.Grids[id]
+	return mp.Grids[int(pos.Y)/mp.GY][(int(pos.X) / mp.GX)]
 }
 
-//GetSurround 获取周围的八个格子,注意对于例如最边上的一圈格子来说,可能存在邻居为nil的情况
-func (mp *MapInfo)GetSurround(self *object.Grid)[8]*object.Grid{
-	var ret [8]*object.Grid
-	//分别为(-1,1) (0,1) (1,1) (-1,0) (1,0) (-1,-1) (0,-1) (1,-1),也就是从上到下,从左到右除了自己的集合,它们分别跟本格子id差了
-	//		NGX-1  NGX	NGX+1  -1	  1		-NGX-1	-NGX   -NGX+1
-	ret[0] = mp.GetGridById(self.Id+ mp.NGX-1)
-	ret[1] = mp.GetGridById(self.Id+ mp.NGX)
-	ret[2] = mp.GetGridById(self.Id+ mp.NGX+1)
-	ret[3] = mp.GetGridById(self.Id-1)
-	ret[4] = mp.GetGridById(self.Id+1)
-	ret[5] = mp.GetGridById(self.Id- mp.NGX-1)
-	ret[6] = mp.GetGridById(self.Id- mp.NGX)
-	ret[7] = mp.GetGridById(self.Id- mp.NGX+1)
-	return ret
+func (mp *MapInfo) GetGridByIdx(x, y int) *object.Grid {
+	if !mp.checkIdxLegal(x, y) {
+		//logger.Errorf("grid[%d][%d] is illegal",y,x)
+		return nil
+	}
+	return mp.Grids[y][x]
+}
+
+func (mp *MapInfo) checkIdxLegal(x, y int) bool {
+	return !(x < 0 || y < 0 || x >= mp.NGX || y >= mp.NGY)
+}
+
+//checkPositionLegal 判断当前位置是否合法,注意例如我们的地图x最大值是40,那么40.2 or 40是违法的位置
+func (mp *MapInfo) checkPositionLegal(pos *entity.Vector2) bool {
+	return !(pos.X < 0 || int(pos.X) >= mp.MX || pos.Y < 0 || int(pos.Y) >= mp.MY)
 }
 
 //NewAOI 创建一个AOI模块
-func NewAOI(heroesInitInfo *HeroesInitInfo,mx,my,gx,gy int,updateDuration time.Duration)*AOI{
-	if mx<0 || my<0 || gx<0 || gy<0{
+func NewAOI(heroesInitInfo *HeroesInitInfo, mx, my, gx, gy int, updateDuration time.Duration) *AOI {
+	if mx < 0 || my < 0 || gx < 0 || gy < 0 {
 		return nil
 	}
 
@@ -85,27 +87,22 @@ func NewAOI(heroesInitInfo *HeroesInitInfo,mx,my,gx,gy int,updateDuration time.D
 		MY:    my,
 		GX:    gx,
 		GY:    gy,
-		NGX:   mx/gx + 1,		//例如mx为10,gx为6,那么x方向应有两个格子
+		NGX:   mx/gx + 1, //例如mx为10,gx为6,那么x方向应有两个格子
 		NGY:   my/gy + 1,
 		NGrid: (mx/gx + 1) * (my/gy + 1),
 	}
 
+	logger.Infof("aoi完成地图的初始化:%+v", *mapInfo)
 	aoi.MapInfo = mapInfo
 	//初始化grids,创建NGrid个数的格子
 	id := 0
-	aoi.Grids = make([]*object.Grid,mapInfo.NGrid)
-	for y := 0; y < mapInfo.NGY; y++ {
-		for x := 0; x < mapInfo.NGX; x++ {
-			id = y*mapInfo.NGX + x
-			aoi.Grids[id] = object.NewGrid(id,x,y)
-		}
+	aoi.Grids = make([][]*object.Grid, mapInfo.NGY)
+	for i := 0; i < mapInfo.NGY; i++ {
+		aoi.Grids[i] = make([]*object.Grid, mapInfo.NGX)
 	}
-	//初始化格子的周围的格子
 	for y := 0; y < mapInfo.NGY; y++ {
 		for x := 0; x < mapInfo.NGX; x++ {
-			id = y*mapInfo.NGX + x
-			aoi.Grids[id].Surround = aoi.GetSurround(aoi.Grids[id])
-			aoi.Grids[id].Grids = aoi.Grids
+			aoi.Grids[y][x] = object.NewGrid(id, x, y, aoi.Grids)
 		}
 	}
 
@@ -113,52 +110,74 @@ func NewAOI(heroesInitInfo *HeroesInitInfo,mx,my,gx,gy int,updateDuration time.D
 	heroInfo := &HeroInfo{
 		HeroNum: len(heroesInitInfo.heroes),
 		Speed:   heroesInitInfo.Speed,
-		heroes:  make(map[int32]*Hero,len(heroesInitInfo.heroes)),
+		Heroes:  make(map[int32]*Hero, len(heroesInitInfo.heroes)),
 	}
 	for i := 0; i < heroInfo.HeroNum; i++ {
-		hero := heroesInitInfo.heroes[i]
-		heroInfo.heroes[hero.Id] = NewHero(hero.Id,hero.Position,entity.Vector2Zero,heroInfo.Speed,aoi)
+		singleInfo := heroesInitInfo.heroes[i]
+		if hero := NewHero(singleInfo.Id, singleInfo.Position, singleInfo.Direction, heroInfo.Speed, aoi); hero == nil {
+			logger.Errorf("向aoi插入英雄的时候出现错误,请检查英雄是否在地图边界处/超出地图位置.hero position:%+v,id:%d", singleInfo.Position, singleInfo.Id)
+		} else {
+			heroInfo.Heroes[hero.Id] = hero
+		}
 	}
 	heroInfo.UpdateDuration = updateDuration
 	aoi.HeroInfo = heroInfo
 
+	aoi.MsgChan = new(MsgChan)
+	aoi.Quit = make(chan *HeroQuitMsg)
+	aoi.Move = make(chan *HeroMoveMsg)
+	aoi.Finish = make(chan struct{})
 	return aoi
 }
 
-//DeleteHero 删除英雄
-func (aoi *AOI)DeleteHero(id int32){
-	aoi.heroLock.Lock()
-	defer aoi.heroLock.Unlock()
-	delete(aoi.heroes, id)
-}
+func (aoi *AOI) UpdateHeroPosition(info *HeroMoveMsg) {
+	hero := aoi.Heroes[info.Id]
 
-func (aoi *AOI)UpdateHeroPosition(info *HeroMovementInfo){
-	aoi.heroLock.RLock()
-	hero := aoi.heroes[info.Id]
-	aoi.heroLock.RUnlock()
-
-	if hero == nil{
-		logger.Errorf("aoi收到了不存在的英雄位置更新信息:hero num:%d",info.Id)
+	if hero == nil {
+		logger.Errorf("aoi收到了不存在的英雄位置更新信息:hero num:%d", info.Id)
 		return
 	}
 	hero.UpdateMovement(info)
 }
 
-func (aoi *AOI)Work(){
+//Work aoi模块开始工作,每秒12帧的发送位置信息
+func (aoi *AOI) Work() {
 	ticker := time.NewTicker(aoi.UpdateDuration)
+	var quitMsg *HeroQuitMsg
+	var moveMsg *HeroMoveMsg
+	var hero *Hero
+	var ok bool
 	go func() {
-		for range ticker.C{
-			aoi.heroLock.RLock()
-			for _, hero := range aoi.heroes {
-				hero.UpdateMovement(nil)
+		for {
+			select {
+			case <-ticker.C:
+				//定时更新英雄的位置信息,并且广播给其它的玩家
+				for _, hero = range aoi.Heroes {
+					hero.UpdateMovement(nil)
+				}
+			case moveMsg = <-aoi.Move:
+				hero, ok = aoi.Heroes[moveMsg.Id]
+				if !ok {
+					logger.Errorf("收到了已经退出/不存在的heroId", moveMsg.Id)
+					continue
+				} else {
+					hero.UpdateMovement(moveMsg)
+				}
+			case quitMsg = <-aoi.Quit:
+				//英雄退出
+				delete(aoi.Heroes, int32(*quitMsg))
+			case <-aoi.Finish:
+				//结束
+				logger.Infof("aoi模块结束工作")
+				return
 			}
-			aoi.heroLock.Unlock()
 		}
 	}()
-	aoi.ticker = ticker
+	aoi.Ticker = ticker
 }
 
-func (aoi *AOI)Stop(){
-	aoi.ticker.Stop()
+//Stop 停止aoi模块的运作
+func (aoi *AOI) Stop() {
+	aoi.Ticker.Stop()
+	close(aoi.Finish)
 }
-
--- plugins/aoi/aoi_test.go
+++ plugins/aoi/aoi_test.go
@@ -1,6 +1,10 @@
 package aoi
 
-import "testing"
+import (
+	"melee_game_server/framework/entity"
+	"testing"
+	"time"
+)
 
 /**
 *@Author Sly
@@ -9,12 +13,31 @@ import "testing"
 *@Description:
  */
 
-func TestNil(t *testing.T) {
-	type S struct {
-		int
+//TestMeet 英雄0从(0,0.1)[0][0]向右走,英雄1从(0,4.9)[0][4]向左走,速度为1m/s(aka 0.001m/ms),
+//应该是	1s:		 (0,1.1)[0][1]			 (0,3.9)[0][3]
+//		2s:		 (0,2.1)[0][2]->进入视野	 (0,2.9)[0][2]
+//		3s:		 (0,3.1)[0][3]			 (0,1.9)[0][1]->离开视野
+//		4s:		 (0,4.1)[0][4]	 		 (0,0.9)[0][0]
+func TestSingleMeet(t *testing.T) {
+	var testHeroInitInfo = &HeroesInitInfo{
+		Speed: 0.001,
+		heroes: []*HeroMoveMsg{
+			{
+				Id:        0,
+				Position:  entity.NewVector2(0.1, 0), //在(0,0)处
+				Direction: entity.Vector2Right,       //往右走
+				Time:      time.Now(),
+			},
+			{
+				Id:        1,
+				Position:  entity.NewVector2(4.9, 0), //在(0,40)处
+				Direction: entity.Vector2Left,        //往左走
+				Time:      time.Now(),
+			},
+		},
 	}
-	var ss [10]*S
-	if ss[0] != nil{
-		t.Fatalf("not nil")
-	}
+	aoi := NewAOI(testHeroInitInfo, 40, 80, 1, 1, 1*time.Second)
+	aoi.Work()
+	time.Sleep(6 * time.Second)
+	aoi.Stop()
 }
